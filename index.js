cc1861aa976a434a1b20c9526dd0666581633e0a
wooyangee@gmail.com <wooyangee@gmail.com>
2018-03-30 06:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ var height = "74"
 module.exports = { name, height
 }
 
-if (name === "Susan") {
-    console.log("The name is correct")
-}
+
+
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
